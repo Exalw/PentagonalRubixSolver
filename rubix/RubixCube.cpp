cf6497a4dd2e0443b39bddab2ad043fe75cdf483
alwan101 <alwan101@uni-duesseldorf.de>
2022-10-15 22:39:52 +0200

    Added global variables with hardcoded coordinates for new Method. Added new Method setCoords() for assigning the coordinates to be changed in updateOtherSidesC(), with support for cc turning. Changed the order of methods according to their hierarchy.

--- rubix/RubixCube.cpp
+++ rubix/RubixCube.cpp
@@ -42,46 +42,37 @@ void RubixCube::emptyBuffer() {
 	pSides->middleColors = new uint16_t[5];
 }
 
-void RubixCube::updateOtherSidesC(int side) {
-	int *pX,*pS;
-	if (side < 1) return;
-	else if (side == 1) {
-		int x[5] = {0, 0, 0, 0, 0};
-		pX = x;
-		int s[5] = {2, 3, 4, 5, 6};
-		pS = s;
-	} else if (side < 7) {
-		int x[5] = {side%5, 4, 4, 0, 1};
-		pX = x;
-		int s[5] = {1, (side - 2 - 1 + 5) % 5 + 2, (side - 7 + 4 + 5) % 5 + 7,
-		            (side - 7 + 5 + 5) % 5 + 7, (side - 2 + 1 + 5) % 5 + 2};
-		pS = s;
-	} else if (side < 12){
-		int x[5] = {-side+11, 1, 2, 3, 3};
-		pX = x;
-		int s[5] = {12, (side - 7 + 1 + 5) % 5 + 7, (side - 2 - 4 + 5) % 5 + 2,
-		            (side - 2 - 5 + 5) % 5 + 2, (side - 7 - 1 + 5) % 5 + 7};
-		pS = s;
-	}
-	else if(side==12) {
-		int x[5] = {2, 2, 2, 2, 2};
-		pX = x;
-		int s[5] = {11, 10, 9, 8, 7};
-		pS = s;
-	}
-	turnEdgesCw(pS, pX);
-	return;
-}
+void RubixCube::setCoords(int *pS, int *pX, bool cw, int side){
+	int mode=0;
+	if(side<1) return;
+	else if(side==1) mode=1;
+	else if(side<7) {
+		mode = 2;
+		x[2-1][0]=side%5;
+		s[2-1][1]=(side - 2 - 1 + 5) % 5 + 2;
+		s[2-1][2]=(side - 7 + 4 + 5) % 5 + 7;
+		s[2-1][3]=(side - 7 + 5 + 5) % 5 + 7;
+		s[2-1][4]=(side - 2 + 1 + 5) % 5 + 2;
+	} else if(side<12) {
+		mode = 3;
+		x[3-1][0]=-side+11;
+		s[3-1][1]=(side - 7 + 1 + 5) % 5 + 7;
+		s[3-1][2]=(side - 2 - 4 + 5) % 5 + 2;
+		s[3-1][3]=(side - 2 - 5 + 5) % 5 + 2;
+		s[3-1][4]=(side - 7 - 1 + 5) % 5 + 7;
+	} else if(side==12) mode=4;
 
-void RubixCube::turnEdgesCw(int* s, int* x){
-	moveEdgeCw(s[0], x[0], 0, 0);
-	moveEdgeCw(s[4], x[4], s[0], x[0]);
-	moveEdgeCw(s[3], x[3], s[4], x[4]);
-	moveEdgeCw(s[2], x[2], s[3], x[3]);
-	moveEdgeCw(s[1], x[1], s[2], x[2]);
-	moveEdgeCw(0, 0, s[1], x[1]);
-	emptyBuffer();
-	return;
+	if (cw) {
+		for (int i = 0; i < 5; i++) {
+			pS[i] = s[mode - 1][i];
+			pX[i] = x[mode - 1][i];
+		}
+	} else {
+		for (int i = 0; i < 5; i++) {
+			pS[4 - i] = s[mode - 1][i];
+			pX[4 - i] = x[mode - 1][i];
+		}
+	}
 }
 
 void RubixCube::moveEdgeCw(int data, int x0, int target, int y0){
@@ -90,6 +81,25 @@ void RubixCube::moveEdgeCw(int data, int x0, int target, int y0){
 	pSides[target].cornerColors[y0] = pSides[data].cornerColors[x0];
 	pSides[target].middleColors[y0] = pSides[data].middleColors[x0];
 	pSides[target].cornerColors[y1] = pSides[data].cornerColors[x1];
+}
+
+void RubixCube::turnEdgesCw(int* pS, int* pX){
+	moveEdgeCw(pS[0], pX[0], 0, 0);
+	moveEdgeCw(pS[4], pX[4], pS[0], pX[0]);
+	moveEdgeCw(pS[3], pX[3], pS[4], pX[4]);
+	moveEdgeCw(pS[2], pX[2], pS[3], pX[3]);
+	moveEdgeCw(pS[1], pX[1], pS[2], pX[2]);
+	moveEdgeCw(0, 0, pS[1], pX[1]);
+	emptyBuffer();
+	return;
+}
+
+void RubixCube::updateOtherSides(int side, bool cw) {
+	int *pX = new int[5];
+	int *pS = new int[5];
+	setCoords(pS,pX,cw,side);
+	turnEdgesCw(pS, pX);
+	return;
 }
 
 void RubixCube::turnSideC(int side){
@@ -105,7 +115,7 @@ void RubixCube::turnSideC(int side){
 	tmpMs[4] = rS.middleColors[0];
 	rS.cornerColors = tmpCs;
 	rS.middleColors = tmpMs;
-	updateOtherSidesC(side);
+	updateOtherSides(side, true);
 }
 
 void RubixCube::turnSideCC(int side){
@@ -121,7 +131,7 @@ void RubixCube::turnSideCC(int side){
 	tmpMs[0] = rS.middleColors[4];
 	rS.cornerColors = tmpCs;
 	rS.middleColors = tmpMs;
-
+	updateOtherSides(side, false);
 }
 
 void RubixCube::printCube() {
--- rubix/RubixCube.h
+++ rubix/RubixCube.h
@@ -17,14 +17,23 @@
 class RubixCube {
 private:
 	RubixSide *pSides = new RubixSide[13];
+	int x[4][5] = {{0, 0, 0, 0, 0},
+				   {-1, 4, 4, 0, 1},
+				   {-1, 1, 2, 3, 3},
+				   {2, 2, 2, 2, 2}};
+	int s[4][5] = {{2, 3, 4, 5, 6},
+				   {1, -1, -1,-1, -1},
+				   {12, -1, -1,-1, -1},
+				   {11, 10, 9, 8, 7}};
 
 public:
 	void init(uint16_t* alNbrs);
 
 	void emptyBuffer();
-	void updateOtherSidesC(int side);
-	void turnEdgesCw(int *s, int *x);
+	void setCoords(int *pS, int *pX, bool cw, int s);
 	void moveEdgeCw(int data, int x0, int target, int x1);
+	void turnEdgesCw(int *pS, int *pX);
+	void updateOtherSides(int side, bool cw);
 	void turnSideC(int side);
 	void turnSideCC(int side);
 
